e5a38091a2c823963fc220aac6bf636d2ef2d41c
Utsav Raychaudhuri <agileutsav@gmail.com>
2020-11-29 18:40:18 -0800

    working commit - Created using LeetHub

---
+++ powx-n.py
@@ -0,0 +1,14 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def rec(x,pows):
+            if pows==0:
+                return 1
+            half=rec(x,pows//2)
+            if pows%2==0:
+                return half*half
+            return half*half*x
+        if n<0:
+            x=1/x
+            n=-n
+        return rec(x,n)
+        
