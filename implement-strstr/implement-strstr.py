e1ebbf22c5feb760ba3bd2c6c15d73b29e0e170b
Utsav Raychaudhuri <agileutsav@gmail.com>
2020-12-12 02:34:08 -0800

    Time: 44 ms (20.35%), Space: 15.3 MB (5.45%) - LeetHub

---
+++ implement-strstr/implement-strstr.py
@@ -0,0 +1,39 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        def lps(needle):
+            lpsarr=[0]*len(needle)
+            j=0
+            i=1
+            while(i<len(needle)):
+                if(needle[i]==needle[j]):
+                    j+=1
+                    lpsarr[i]=j
+                    i+=1
+                elif needle[i]!=needle[j] and j>0:
+                    j=lpsarr[j-1]
+                else:
+                    lpsarr[i]=0
+                    i+=1
+            return lpsarr
+        if len(needle)==0:
+            return 0
+        m=len(haystack)
+        n=len(needle)
+        i,j=0,0
+        lps=lps(needle)
+        if(m<n):
+            return -1
+        while(i<m):
+            if haystack[i]==needle[j]:
+                i+=1
+                j+=1
+                if j==n:
+                    return i-n
+            elif i<m and j>0 and haystack[i]!=needle[j]:
+                j=lps[j-1]
+            elif i<m and j==0 and haystack[i]!=needle[j]:
+                i+=1
+        return -1
+                
+                
+            
