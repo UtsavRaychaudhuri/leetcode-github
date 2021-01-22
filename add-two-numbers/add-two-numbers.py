b502bd2cd39d51bcddca5e0a9388c5983b4d4ada
Utsav Raychaudhuri <agileutsav@gmail.com>
2021-01-21 17:41:15 -0800

    Time: 72 ms (51.16%), Space: 14.3 MB (73.26%) - LeetHub

--- add-two-numbers/add-two-numbers.py
+++ add-two-numbers/add-two-numbers.py
@@ -1,32 +1,38 @@
-#Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-​
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        a=""
-        b=""
-        head=l1
-        head1=l2
-        while l1!=None:
-            a= str(l1.val) + a
-            l1 = l1.next
-​
-        while l2!=None:
-            b= str(l2.val) + b
-            l2 = l2.next
-​
-        total = str(int(a)+int(b))
-        head,new_node = None,None
-        for i in total:
-            if head is None:
-                head = ListNode(int(i))
-            else:
-                new_node = ListNode(int(i))
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        head=ListNode()
+        tmp=head
+        carry=0
+        while(l1 or l2):
+            if l1 and l2:
+                tmp.next=ListNode((l1.val+l2.val+carry)%10)
+                if l1.val+l2.val+carry>9:
+                    carry=1
+                else:
+                    carry=0
+                tmp,l1,l2=tmp.next,l1.next,l2.next
+            elif l1:
+                tmp.next=ListNode((l1.val+carry)%10)
+                if l1.val+carry>9:
+                    carry=1
+                else:
+                    carry=0
+                tmp,l1=tmp.next,l1.next
+            elif l2:
+                tmp.next=ListNode((l2.val+carry)%10)
+                if l2.val+carry>9:
+                    carry=1
+                else:
+                    carry=0
+                tmp,l2=tmp.next,l2.next
+        if carry>0:
+            tmp.next=ListNode(1)
+        return head.next
+                
+                
+        
