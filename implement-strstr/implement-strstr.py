616fad9c1fca6cdc41516344e1f1d5621287b274
Utsav Raychaudhuri <agileutsav@gmail.com>
2021-02-10 19:13:46 -0800

    Time: 24 ms (96.83%), Space: 14.3 MB (92.20%) - LeetHub

--- implement-strstr/implement-strstr.py
+++ implement-strstr/implement-strstr.py
@@ -1,39 +1,11 @@
 class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        def lps(needle):
-            lpsarr=[0]*len(needle)
-            j=0
-            i=1
-            while(i<len(needle)):
-                if(needle[i]==needle[j]):
-                    j+=1
-                    lpsarr[i]=j
-                    i+=1
-                elif needle[i]!=needle[j] and j>0:
-                    j=lpsarr[j-1]
-                else:
-                    lpsarr[i]=0
-                    i+=1
-            return lpsarr
-        if len(needle)==0:
-            return 0
-        m=len(haystack)
-        n=len(needle)
-        i,j=0,0
-        lps=lps(needle)
-        if(m<n):
-            return -1
-        while(i<m):
-            if haystack[i]==needle[j]:
-                i+=1
-                j+=1
-                if j==n:
-                    return i-n
-            elif i<m and j>0 and haystack[i]!=needle[j]:
-                j=lps[j-1]
-            elif i<m and j==0 and haystack[i]!=needle[j]:
-                i+=1
-        return -1
-                
-                
-            
+    def strStr(self, haystack: str, needle: str) -> int:
+        if not needle:
+            return 0
+        for i in range((len(haystack)-len(needle))+1):
+            if haystack[i]==needle[0] and haystack[i:i+len(needle)]==needle:
+                    return i
+        return -1
+        
+                    
+                    
