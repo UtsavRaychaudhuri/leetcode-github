f394dbe195d009d515a8503e2f2e2ad898e89ca2
Utsav Raychaudhuri <agileutsav@gmail.com>
2021-02-19 17:33:36 -0800

    Time: 92 ms (70.60%), Space: 15.5 MB (67.99%) - LeetHub

--- graph-valid-tree/graph-valid-tree.py
+++ graph-valid-tree/graph-valid-tree.py
@@ -1,18 +1,31 @@
 import collections
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n - 1: 
+            return False
         parent=[i for i in range(n)]
+        size=[1]*n
         def find(x):
-            if parent[x]!=x:
-                parent[x]=find(parent[x])
-            return parent[x]
-        for i,v in edges:
-            parent_i=find(i)
-            parent_v=find(v)
-            if parent_i==parent_v:
-                return False
-            parent[parent_i]=parent[parent_v]
-        return len(set(find(i) for i in range(n)))==1
+            root=x
+            while parent[root]!=root:
+                root =parent[root]
+            while x!=root:
+                old=parent[x]
+                parent[x]=root
+                x=old
+            return root
+        for e1,e2 in edges:
+            parent1=find(e1)
+            parent2=find(e2)
+            if parent1==parent2:
+                return False     
+            if size[parent1]<size[parent2]:
+                parent[parent1]=parent2
+                size[parent2]+=size[parent1]
+            else:
+                parent[parent2]=parent1
+                size[parent1]+=size[parent2]
+        return True
             
             
             
