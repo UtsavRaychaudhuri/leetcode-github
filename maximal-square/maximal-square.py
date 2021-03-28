53a073ac24119c51d220fbde23e38e5a8a9d9551
Utsav Raychaudhuri <agileutsav@gmail.com>
2021-03-28 12:26:13 -0700

    Time: 868 ms (5.01%), Space: 15.6 MB (29.92%) - LeetHub

--- maximal-square/maximal-square.py
+++ maximal-square/maximal-square.py
@@ -1,20 +1,29 @@
-class Solution(object):
-    def maximalSquare(self, matrix):
-        """
-        :type matrix: List[List[str]]
-        :rtype: int
-        """
-        dp=[]
-        gm=0
-        for i in range(1,len(matrix)):
-            for j in range(1,len(matrix[0])):
-                if matrix[i][j]=="1":
-                    matrix[i][j]=min(int(matrix[i-1][j]),int(matrix[i-1][j-1]),int(matrix[i][j-1]))+1
-                gm=max(gm,int(matrix[i][j]))
-        if gm==0:
-            for i in matrix:
-                if "1" in i:
-                    return 1
-        return pow(gm,2)
-                
-            
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        res=0
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j]=="1":
+                    k,l=i+1,j+1
+                    flag=True
+                    res=max(res,1)
+                    while(k<len(matrix) and l<len(matrix[0]) and matrix[k][l]=="1"):
+                        for m in range(k,i-1,-1):
+                            if matrix[m][l]!="1":
+                                flag=False
+                                break
+                        if not flag:
+                            break
+                        for n in range(l,j-1,-1):
+                            if matrix[k][n]!="1":
+                                flag=False
+                                break
+                        if not flag:
+                            break
+                        else:
+                            res=max(k-i+1,res)
+                            k+=1
+                            l+=1
+        if res==0:
+            return 0        
+        return (res)**2               
