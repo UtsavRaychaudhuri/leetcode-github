8442f3d457bee848a794ab8e778ff2744fccfce7
Utsav Raychaudhuri <agileutsav@gmail.com>
2021-03-28 22:00:00 -0700

    Time: 40 ms (59.87%), Space: 14.6 MB (24.65%) - LeetHub

--- word-break/word-break.py
+++ word-break/word-break.py
@@ -1,18 +1,14 @@
 class Solution:
-    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        my_set=set()
-        for i in wordDict:
-            my_set.add(i)
-        wordnow=""
-        dp=[0]
-        for i in range(len(s)+1):
-            for j in dp:
-                if s[j:i] in my_set:
-                    dp.append(i)
-                    break
-        if dp[-1]==len(s):
-            return True
-        return False
-                
-            
-        
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordSet=set(wordDict)
+        @lru_cache(maxsize=None)
+        def backtrack(s):
+            if len(s)==0:
+                return True
+            for i in range(len(s)):
+                if s[:i+1] in wordSet and backtrack(s[i+1:]):
+                    return True
+            return False
+        return backtrack(s)
+        
+                
