7821ffd9dfc3f652be6792e96772a63cf5ec2679
Utsav Raychaudhuri <agileutsav@gmail.com>
2021-03-30 19:21:38 -0700

    Time: 284 ms (62.91%), Space: 14.8 MB (84.17%) - LeetHub

--- the-maze/the-maze.py
+++ the-maze/the-maze.py
@@ -1,14 +1,28 @@
 class Solution:
-    def hasPath(self, maze, start, destination) -> bool:
-        m, n, seen = len(maze), len(maze[0]), set()
-        def dfs(i, j):
-            if [i, j] == destination: return True
-            for dx, dy in ((0,-1),(0,1),(-1,0),(1,0)):
-                x, y = i, j
-                while 0 <= x+dx < m and 0 <= y+dy < n and not maze[x+dx][y+dy]:
-                    x, y = x+dx, y+dy
-                if (x,y) not in seen: 
-                    seen.add((x,y))
-                    if dfs(x,y): return True
-            return False
-        return dfs(*start)
+    def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
+        if start==destination:
+            return True
+        q=collections.deque([])
+        q.append(start)
+        dirs=[[0,1],[-1,0],[0,-1],[1,0]]
+        while(q):
+            ele=q.popleft()
+            if ele==destination:
+                return True
+            for i,j in dirs:
+                k,l=ele[0]+i,ele[1]+j
+                while(k>=0 and k<len(maze) and l>=0 and l<len(maze[0]) and maze[k][l]!=1):
+                    k+=i
+                    l+=j
+                k-=i
+                l-=j
+                if maze[k][l]!=2:
+                    q.append([k,l])
+                    maze[k][l]=2
+        return False
+                
+                    
+                    
+                
+                
+            
