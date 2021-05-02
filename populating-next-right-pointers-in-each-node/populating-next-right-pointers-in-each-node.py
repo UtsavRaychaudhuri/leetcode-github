f001c580a6d3e97c464e168f70c8ef79ec02146e
Utsav Raychaudhuri <agileutsav@gmail.com>
2021-05-02 11:02:43 -0700

    Time: 68 ms (27.20%), Space: 15.6 MB (91.31%) - LeetHub

--- populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
+++ populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -1,27 +1,24 @@
 """
 # Definition for a Node.
 class Node:
-    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
-        self.val = val
-        self.left = left
-        self.right = right
-        self.next = next
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
 """
-​
+
 class Solution:
-    def connect(self, root: 'Node') -> 'Node':
-        def conn(root,arr,i):
-            if root is None:
-                return
-            if i<len(arr) and arr[i]!=root:
-                arr[i].next=root
-                arr[i]=root
-            else:
-                arr.append(root)
-            conn(root.left,arr,i+1)
-            conn(root.right,arr,i+1)
-        conn(root,[],0)
-        return root
-        
-        
-        
+    def connect(self, root: 'Node') -> 'Node':
+        def conn(root,l,arr):
+            if root is None:
+                return
+            if l==len(arr):
+                arr.append(root)
+            else:
+                arr[l].next=root
+                arr[l]=root
+            conn(root.left,l+1,arr)
+            conn(root.right,l+1,arr)
+        conn(root,0,[])
+        return root
