506ef68ac56351c114a2e836a1f1c5c0de1d537a
Utsav Raychaudhuri <agileutsav@gmail.com>
2021-05-02 10:37:15 -0700

    Time: 76 ms (40.97%), Space: 29.7 MB (6.00%) - LeetHub

--- lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -9,25 +9,22 @@ class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         self.pathtop=[]
         self.pathtoq=[]
-        def pathtonode(root,path):
+        def lca(root,p,q,arr):
             if root is None:
                 return
-            path.append(root)
-            if root==p:
-                self.pathtop=path.copy()
+            arr.append(root)
             if root==q:
-                self.pathtoq=path.copy()
-            pathtonode(root.left,path)
-            pathtonode(root.right,path)
-            path.pop()
-        pathtonode(root,[])
+                self.pathtoq=arr.copy()
+            if root==p:
+                self.pathtop=arr.copy()
+            lca(root.left,p,q,arr)
+            lca(root.right,p,q,arr)
+            arr.pop()
+        lca(root,p,q,[])
         for i in range(min(len(self.pathtop),len(self.pathtoq))):
             if self.pathtop[i]!=self.pathtoq[i]:
                 return self.pathtop[i-1]
-        return self.pathtop[i]
-    
+        return self.pathtop[min(len(self.pathtop),len(self.pathtoq))-1]
             
+                       
                 
-                    
-                
-        
